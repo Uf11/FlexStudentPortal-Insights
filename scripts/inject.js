0b8503c0248625c58075926d021066a1efbecb0e
Uf11 <itsusman11@gmail.com>
2024-05-22 03:05:10 +0500

    Update inject.js

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,82 +1,76 @@
-load()
+setTimeout(function () {
+    load();
+}, 1000); // 1000 milliseconds = 1 second
 
-function load() {
-    preload();
-    let tabs = document.getElementsByClassName("nav-item m-tabs__item");
-    // when any tab is clicked, console.log the tab's id
-    for (let i = 0; i < tabs.length; i++) {
-        tabs[i].addEventListener("click", function () {
-            preload();
+function ftn_calculateMarks(id) {
+    var temp = "totalColumn_" + id;
+    var tempGrandObtMarks = 0;
+    $('.sum_table').each(function (t) {
+
+        $(this).find("." + temp).each(function () {
+            $(this).find(".totalColObtMarks").each(function () {
+                if ($(this).text() != "") {
+                    tempGrandObtMarks += parseFloat($(this).text());
+                }
+            });
+
         });
-    }
 
+    });
+    // Check if there is already a value and it meets certain conditions before updating
+    if ((!isNaN(tempGrandObtMarks) && tempGrandObtMarks != 0 && !$("#GrandtotalObtMarks_" + id).html())) {
+        // Update the HTML content only if there is no existing value
+        $("#GrandtotalObtMarks_" + id).html(tempGrandObtMarks.toFixed(2));
+    }
 }
 
-function preload() {
-    // extract the semid from the url
-    let semid = window.location.href.split("=")[1];
+
+function load() {
 
     // for every tab-pane
     let tabPanes = document.getElementsByClassName("tab-pane");
     for (let i = 0; i < tabPanes.length; i++) {
         let innerHTML = tabPanes[i].innerHTML;
         let startIndex = innerHTML.indexOf("ftn_calculateMarks(");
-        let endIndex = innerHTML.indexOf(");", startIndex);
+        let endIndex = innerHTML.indexOf(")", startIndex);
         let substring = innerHTML.substring(startIndex, endIndex);
         let tabPaneID = parseInt(substring.replace("ftn_calculateMarks(", "").replaceAll("'", ""));
+        // get id of the current tab-pane
+        var courseCode = $(tabPanes[i]).attr('id');
+        console.log("courseCode", courseCode, "tabPaneID", tabPaneID);
 
-        // remove any button with id "get-insights-${tabPaneID}"
-        let prevButton = document.getElementById(`get-insights-${tabPaneID}`);
-        if (prevButton) {
-            prevButton.remove();
+        if (isNaN(tabPaneID)) {
+            console.log("tabPaneID is NaN", tabPaneID);
+            continue;
         }
 
-        // if previous insights exist, remove them
-        let prevInsights = document.getElementsByClassName("insights")[0];
-        if (prevInsights) {
-            prevInsights.remove();
+        // Construct the ID of the <td> element to check for existence
+        var tdIdToCheck = "GrandtotalClassAvg_" + tabPaneID;
+
+        // Check if a <td> element with the specified ID exists in the tbody
+        var tdExists = $('#' + courseCode + "-Grand_Total_Marks").find('table tbody').find('td#' + tdIdToCheck).length > 0;
+
+        // If the <td> element exists, return without appending new content
+        if (tdExists) {
+            return;
         }
 
-        // prepend a button to the tab-pane called "Get Insights"
-        tabPanes[i].innerHTML = `<button class="btn btn-primary" id="get-insights-${tabPaneID}" style="margin-bottom: 2rem">Get Insights</button>` + tabPanes[i].innerHTML;
+        // Construct the new <tr> element
+        var newTableRow =
+            '<tr class="totalColumn_' + tabPaneID + '">' +
+            '<td class="text-center" id="GrandtotalColMarks_' + tabPaneID + '"></td>' +
+            '<td class="text-center" id="GrandtotalObtMarks_' + tabPaneID + '"></td>' +
+            '<td class="text-center" id="GrandtotalClassAvg_' + tabPaneID + '"></td>' +
+            '<td class="text-center" id="GrandtotalClassMin_' + tabPaneID + '"></td>' +
+            '<td class="text-center" id="GrandtotalClassMax_' + tabPaneID + '"></td>' +
+            '<td class="text-center" id="GrandtotalClassStdDev_' + tabPaneID + '"></td>' +
+            '</tr>';
 
-        // add an event listener to the button
-        let button = document.getElementById(`get-insights-${tabPaneID}`);
-        button.addEventListener("click", function () {
-                
-                $.ajax
-                    ({
-                        type: "POST",
-                        url: "../Student/GetClassAvg",
-                        data: {
-                            CourseId: tabPaneID, // nlp - 2034, is - 1472, pp - 1483, ir - 1932
-                            SemID: semid
-                        },
-                        success: function (data) {
-    
-                            // remove any div with class name "insights"
-                            let insights = document.getElementsByClassName("insights")[0];
-                            if (insights) {
-                                insights.remove();
-                            }
-    
-                            let userInsights = data[0];
-                            tabPanes[i].innerHTML = `
-                                <div class="insights" style="margin-bottom: 2rem">
-                                    <h5 style="margin-bottom: 1rem">Insights</h5>
-                                    <p>Class Average: ${userInsights.CLASS_AVG}</p>
-                                    <p>Class Minimum: ${userInsights.CLASS_MIN}</p>
-                                    <p>Class Maximum: ${userInsights.CLASS_MAX}</p>
-                                    <p>Class Standard Deviation: ${userInsights.CLASS_STD}</p>
-                                    <p>Grand Total: ${userInsights.TOT_WEIGHT}</p>
-                                </div>
-                            ` + tabPanes[i].innerHTML;
+        // Append the new <tr> element to the tbody
+        $('#' + courseCode + "-Grand_Total_Marks").find('table tbody').append(newTableRow);
 
-                        }
-                        
-                    });
 
-        });
+        ftn_calculateMarks(tabPaneID);
 
     }
 }
